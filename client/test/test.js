f2eb65bb6c454515289b004b44a992951f5a6758
xevisalle <xavier@dusk.network>
2024-03-21 12:22:45 +0100

    Fix

--- client/test/test.js
+++ client/test/test.js
@@ -10,12 +10,12 @@ const RECEIVER_PK = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
 // Contracts
 const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const EXCHANGE_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const WALLET_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const PACCS_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 // Contracts ABI
 const TOKEN_ABI = JSON.parse(fs.readFileSync("../contracts/out/Token.sol/Token.json")).abi; 
 const EXCHANGE_ABI = JSON.parse(fs.readFileSync("../contracts/out/Exchange.sol/Exchange.json")).abi; 
-const WALLET_ABI = JSON.parse(fs.readFileSync("../contracts/out/Wallet.sol/Wallet.json")).abi;
+const PACCS_ABI = JSON.parse(fs.readFileSync("../contracts/out/Paccs.sol/Paccs.json")).abi;
 
 // Set owner wallet
 const provider = new ethers.providers.JsonRpcProvider();
@@ -35,7 +35,7 @@ describe('PACCs JS Testing Suite', function () {
     it('Buy tokens works.', async () => {
         let token_contract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, wallet);
         let exchange_contract = new ethers.Contract(EXCHANGE_ADDRESS, EXCHANGE_ABI, wallet);
-        let wallet_contract = new ethers.Contract(WALLET_ADDRESS, WALLET_ABI, wallet);
+        let paccs_contract = new ethers.Contract(PACCS_ADDRESS, PACCS_ABI, wallet);
 
         // transfer some tokens to the exchange contract
         await token_contract.transfer(EXCHANGE_ADDRESS, 100000);
@@ -47,7 +47,7 @@ describe('PACCs JS Testing Suite', function () {
         assert(balance.eq(new_balance.add(ethers.BigNumber.from("100000"))));
 
         wallet.sendTransaction({
-            to: WALLET_ADDRESS,
+            to: PACCS_ADDRESS,
             value: 10
         });
 
@@ -56,7 +56,7 @@ describe('PACCs JS Testing Suite', function () {
         await token_contract.transfer(EXCHANGE_ADDRESS, 10);
 
         balance = await token_contract.balanceOf(EXCHANGE_ADDRESS);
-        await wallet_contract.orderExchange(10);
+        await paccs_contract.orderAction(10);
         new_balance = await token_contract.balanceOf(EXCHANGE_ADDRESS);
         assert(balance.eq(new_balance.add(ethers.BigNumber.from("10"))));
     });
--- contracts/README.md
+++ contracts/README.md
@@ -13,7 +13,7 @@ forge install openzeppelin/openzeppelin-contracts --no-git
 
 ## Usage
 
-Our code contains two contracts: the *Exchange* contract that deploys an ERC-20 token, and the *Wallet* contract being the smart contract wallet. You can test them by running:
+Our code contains three contracts: the *Token* contract that deploys an ERC-20 token, the *Exchange* contract that deploys a DEX application, and the *Paccs* contract being our protocol's smart contract. You can test them by running:
 
 ```
 forge test
@@ -30,7 +30,7 @@ Now compile and deploy the contracts in our local devnet, as follows (in that pa
 ```
 forge create Token --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --constructor-args "Token" "TOK"
 forge create Exchange --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --constructor-args "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-forge create Wallet --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --constructor-args "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+forge create Paccs --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --constructor-args "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 ```
 
 You can now test and use the clent using the instructions in the `client` folder.
