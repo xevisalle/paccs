bcbc31e533afac1b352ae748d97df71bad37107a
xevisalle <xavier@dusk.network>
2024-10-28 16:33:13 +0100

    Make use of Poseidon in-contract

--- contracts/src/Paccs.sol
+++ contracts/src/Paccs.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.13;
 
 import "./Exchange.sol";
+import "./poseidon-solidity/contracts/PoseidonT3.sol";
+import "./poseidon-solidity/contracts/PoseidonT4.sol";
 
 contract Paccs {
     struct User {
         uint ether_balance;
         uint tok_balance;
-        bytes32 commitment;
+        uint commitment;
     }
 
+    using PoseidonT3 for uint256[2];
+    using PoseidonT4 for uint256[3];
+
     mapping(address => User) public users;
-    mapping(uint256 => bytes32) public tx_commitments; // S => tx
+    mapping(uint256 => uint) public tx_commitments; // S => tx
 
     Exchange exchange;
     Token token;
@@ -25,9 +30,9 @@ contract Paccs {
 
     receive() external payable {}
 
-    function withdraw_ether(uint _amount, uint256 s, uint256 r, bytes32 commitment) external {
+    function withdraw_ether(uint _amount, uint256 s, uint256 r, uint commitment) external {
         require(users[msg.sender].ether_balance >= _amount, "Not enough balance.");
-        require(users[msg.sender].commitment == keccak256(abi.encode(s, r)) || users[msg.sender].commitment == 0, "Provided opening not correct.");
+        require(users[msg.sender].commitment == [s, r].hash() || users[msg.sender].commitment == 0, "Provided opening not correct.");
         require(tx_commitments[s] == 0, "An existing committed transaction was never issued.");
 
         if(users[msg.sender].ether_balance > collateral) {
@@ -56,7 +61,7 @@ contract Paccs {
         return users[user_address].tok_balance;
     }
 
-    function getUserCommitment(address user_address) external view returns (bytes32) {
+    function getUserCommitment(address user_address) external view returns (uint) {
         return users[user_address].commitment;
     }
 
@@ -66,7 +71,7 @@ contract Paccs {
 
     // PACCS METHODS
     
-    function topUp(bytes32 commitment) public payable {
+    function topUp(uint commitment) public payable {
         users[msg.sender].ether_balance += msg.value;
 
         if(users[msg.sender].ether_balance > collateral && users[msg.sender].commitment == 0) {
@@ -77,14 +82,14 @@ contract Paccs {
     // TODO: check what's needed to prevent relayer spoofing:
     // e.g. sniff S and send it in advance to the network with a
     // random tx_commitment, burning user funds
-    function commitToAction(uint256 s, bytes32 tx_commitment) public {
+    function commitToAction(uint256 s, uint tx_commitment) public {
         tx_commitments[s] = tx_commitment;
     }
 
-    function orderAction(uint256 s, uint256 r, uint _amountToBuy, bytes32 commitment) public {
+    function orderAction(uint256 s, uint256 r, uint _amountToBuy, uint commitment) public {
         require(users[msg.sender].ether_balance >= _amountToBuy, "Not enough Ether to order the action.");
-        require(users[msg.sender].commitment == keccak256(abi.encode(s, r)), "Provided opening not correct.");
-        require(tx_commitments[s] == keccak256(abi.encode(s, r, _amountToBuy)), "Transaction was never committed.");
+        require(users[msg.sender].commitment == [s, r].hash(), "Provided opening not correct.");
+        require(tx_commitments[s] == [s, r, _amountToBuy].hash(), "Transaction was never committed.");
         
         users[msg.sender].ether_balance -= _amountToBuy;
         exchange.buyTokens{value: _amountToBuy}();
